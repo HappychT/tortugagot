bb5d30c41e99e12d98255f0d4db19205b3d84eb0
Shakhmatist <51895021+Shakhmat1st@users.noreply.github.com>
2025-02-01 02:32:35 +0400

    Update GOTItemArmor.java

--- Game-of-Thrones-Main/src/main/java/got/common/item/other/GOTItemArmor.java
+++ Game-of-Thrones-Main/src/main/java/got/common/item/other/GOTItemArmor.java
@@ -1,17 +1,24 @@
 package got.common.item.other;
 
+import got.common.GOTLevelData;
 import got.common.database.GOTCreativeTabs;
+import got.common.enchant.GOTEnchantment;
+import got.common.enchant.GOTEnchantmentHelper;
 import got.common.faction.GOTFaction;
+import got.common.registers.EffectRegister;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.*;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.StringUtils;
+import net.minecraft.world.World;
 
 public class GOTItemArmor extends ItemArmor {
 	public String extraName;
 	public String path;
 	public int slot;
-	public boolean isFactionArmor = false;
-	public GOTFaction faction;
+	int counter = 0;
 
 	public GOTItemArmor(ArmorMaterial material, int slotType) {
 		this(material, slotType, "");
@@ -24,6 +31,18 @@ public class GOTItemArmor extends ItemArmor {
 		extraName = s;
 		slot = slotType;
 	}
+
+	@Override
+	public void onArmorTick(World world, EntityPlayer entityplayer, ItemStack itemstack) {
+		if (GOTEnchantmentHelper.hasEnchant(itemstack, GOTEnchantment.secondBreathBuff) && !entityplayer.isPotionActive(EffectRegister.secondBreath)) {
+				entityplayer.addPotionEffect(new PotionEffect(EffectRegister.secondBreath.id, 200));
+		}
+
+		if (GOTEnchantmentHelper.hasEnchant(itemstack, GOTEnchantment.dexterityBuff) && !entityplayer.isPotionActive(EffectRegister.dexterity)) {
+			entityplayer.addPotionEffect(new PotionEffect(EffectRegister.dexterity.id, 200));
+		}
+	}
+
 
 	public String getArmorName() {
 		String suffix;
