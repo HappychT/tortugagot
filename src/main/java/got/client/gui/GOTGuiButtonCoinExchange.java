84a84bbfa938309284cc55c5154b0656c92c0356
drpgkyoto <senxsoul@gmail.com>
2025-11-01 15:22:12 -0400

    another try32425

--- src/main/java/got/client/gui/GOTGuiButtonCoinExchange.java
+++ src/main/java/got/client/gui/GOTGuiButtonCoinExchange.java
@@ -4,23 +4,22 @@ import org.lwjgl.opengl.GL11;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
-import net.minecraft.util.ResourceLocation;
 
-public class GOTGuiButtonLock extends GuiButton {
-	public static ResourceLocation texture = new ResourceLocation("got:textures/gui/widgets.png");
-
-	public GOTGuiButtonLock(int i, int j, int k) {
-		super(i, j, k, 20, 20, "");
+public class GOTGuiButtonCoinExchange extends GuiButton {
+	public GOTGuiButtonCoinExchange(int i, int j, int k) {
+		super(i, j, k, 32, 17, "");
 	}
 
 	@Override
 	public void drawButton(Minecraft mc, int i, int j) {
 		if (visible) {
-			mc.getTextureManager().bindTexture(texture);
+			mc.getTextureManager().bindTexture(GOTGuiCoinExchange.guiTexture);
 			GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 			field_146123_n = i >= xPosition && j >= yPosition && i < xPosition + width && j < yPosition + height;
 			int k = getHoverState(field_146123_n);
-			drawTexturedModalRect(xPosition, yPosition, 0, 196 + k * 20, width, height);
+			int u = 176 + id * width;
+			int v = 0 + k * height;
+			drawTexturedModalRect(xPosition, yPosition, u, v, width, height);
 			mouseDragged(mc, i, j);
 		}
 	}
